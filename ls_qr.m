663e155340534c1bdcf7a02fce49cb8476bd36f4
NicholasGay <38671804+NicholasGay@users.noreply.github.com>
2018-06-21 19:05:35 +0200

    Add files via upload

--- ls_qr.m
+++ ls_qr.m
@@ -1,4 +1,7 @@
 function x = ls_qr(A,b)
-
-
+n = size(A,2);
+[W,R] = hhqr(A);
+answer = applyQHe(W,b);
+x = upperbacksub(R,answer);
+x(n+1:end,:) = [];
 end
--- ls_svd.m
+++ ls_svd.m
@@ -1,5 +1,13 @@
 function x = ls_svd(A,b)
-
+n = size(A,2);
 [U,Sigma,V] = svd(A);
 
+U = U(:,1:n);
+c = U'*b;
+
+Sigma = Sigma(1:n,1:n);
+
+w = Sigma\c;
+
+x = V*w;
 end
