08cea7bb23b93c712bcddb4b2bbbe00058f4d30d
ga96dof <giulio.evangelisti@tum.de>
2018-06-28 19:00:58 +0200

    Comparison task 3.11 & 3.12

--- Compare.m
+++ Compare.m
@@ -5,8 +5,15 @@ close all
 A = [10^-5, 10^-15; 10^-5, -10^-15; 1, 0.5];
 b = [1;2;0];
 
-X_analytic = [1.5*10^5+10^-5;-3*10^5-2*10^-5];
+% Analytic LS solution
+x_analytic = 1e5*[1.5;-3] + 1e-5*[1;-2] + 1e-15*[0;-4];
 
-X_svd = ls_svd(A,b)
-X_qr = ls_qr(A,b)
-X_cf = ls_cf(A,b)
+% Computation of numerical algorithms' solutions 
+x_svd = ls_svd(A,b);
+x_qr = ls_qr(A,b);
+x_cf = ls_cf(A,b);
+
+% Print errors
+disp(['Error SVD = ', num2str(norm(x_analytic-x_svd))])
+disp(['Error QR = ', num2str(norm(x_analytic-x_qr))])
+disp(['Error Cholesky = ', num2str(norm(x_analytic-x_cf))])
